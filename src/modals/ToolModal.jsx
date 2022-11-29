75d32033a7d4c387c89b849abb340b633a7f9fb8
Meikyo72 <meikyo72@gmail.com>
2022-11-29 19:12:24 +0100

    made changes to modals and kontakt

--- src/modals/ToolModal.jsx
+++ src/modals/ToolModal.jsx
@@ -1,4 +1,4 @@
-import Dtoolbox from '../assets/Dtoolbox.png';
+import Dtoolbox from '../assets/toolbubble.png';
 
 const ToolModal = ({setToolOpen}) => {
     return (
@@ -7,17 +7,19 @@ const ToolModal = ({setToolOpen}) => {
         <div className='modalHeader'>
             <h1>Vår verktygslåda</h1>
         </div>
-        <div className='modalBody'>
-        <div className='modalImage'>
+        <div className='modalBody digitalModalBody'>
+        <div className='digitalModalImage'>
         <img src={Dtoolbox} alt="Dtoolbox" />
         </div>
         <div className='modalText'>
             <p>
             På EnJa AB håller vi oss ständigt uppdaterade vad gäller forskning och har till exempel i frågor om 
             digitalisering ett givande samarbete med ”Digital förvaltning på Göteborgs universitet”. 
-            Vår verktygslåda är full med metoder som är baserade på forskning och best practice. 
+            Vår verktygslåda är full med metoder som är baserade på forskning och best practice.
+            </p>
+            <p> 
             Exempelvis är Effektkartor en väl beprövad metod som vi ofta använder i våra uppdrag. 
-            Metoden används för att bättre förstå vilka effekter som man vill uppnå med en viss tjänst eller produkt. 
+            Metoden används för att bättre förstå vilka effekter som man vill uppnå med en viss förändring, tjänst eller produkt. 
             Effektkartor ger vägledning i hur prioriteringar ska göras för att uppnå önskad effekt. Effektkartor bidrar till att förändringsarbetet får ett tydligt fokus.​
             </p>
         </div>
